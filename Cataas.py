90a2531a45ca964a8057f550a726294ea78d1ea9
Neo <pytandgit@gmail.com>
2025-07-28 10:13:15 +0300

    Создана функция для получения изображения с котиком

--- Cataas.py
+++ Cataas.py
@@ -1,7 +1,19 @@
 from tkinter import *
-from PIL import Image, ImageTK
+from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+def load_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+        return None
+
 
 
 window = Tk()
