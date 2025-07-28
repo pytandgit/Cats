60f35b02b95a8410d1bae848038edb1190aa4263
Neo <pytandgit@gmail.com>
2025-07-28 15:51:16 +0300

    Теперь каждое изображение открывается в новом окне

--- Cataas.py
+++ Cataas.py
@@ -16,11 +16,15 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
 
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
 
 
@@ -31,21 +35,18 @@ window = Tk()
 window.title('Cats!')
 window.geometry('600x520')
 
-label = Label()
-label.pack()
-
-# update_button = Button(text='Обновить', command=set_image)
+# update_button = Button(text='Обновить', command=open_new_window)
 # update_button.pack()
+
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 file_menu = Menu(menu_bar, tearoff=0)
-file_menu.add_cascade(label='Файл', menu=file_menu)
-file_menu.add_command(label='Загрузить фото', command=set_image)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 
 
 url = 'https://cataas.com/cat'
-set_image()
 
 window.mainloop()
